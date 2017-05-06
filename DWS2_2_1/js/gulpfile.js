b7a291f76c013624a25aaf62f3952a81027a6309
William <wsf235@gmail.com>
2017-05-06 15:28:49 -0400

    no message

---
+++ DWS2_2_1/js/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function(){
+
+});
+
+gulp.task('sass', function(){
+    return gulp.src('../css/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('../css'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('../css/*.sass', ['sass'])
+});
