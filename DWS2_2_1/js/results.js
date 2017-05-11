4104aca08687b5d6229285e7a9f3bf319f54cfe8
William <wsf235@gmail.com>
2017-05-11 16:32:38 -0400

    Fixed and improved a few CSS bits while implementing flex box

--- DWS2_2_1/js/results.js
+++ DWS2_2_1/js/results.js
@@ -9,7 +9,6 @@ var template = Handlebars.compile(templateSource);
 
 var result_holder = document.getElementById('search_results');
 
-
 document.getElementById('form_submission').addEventListener('submit', function (e) {
     e.preventDefault();
     Request(document.getElementById('search_input').value);
@@ -47,6 +46,8 @@ function Request(query) {
     url += '?q='+query;
     url += '&type=album';
     url += '&limit=9';
+    document.getElementById('top').innerHTML += "<h2>Showing results for "+query+"</h2>";
+
 
     request.open('GET', url, true);
 
@@ -54,14 +55,13 @@ function Request(query) {
         if (request.status >= 200 && request.status < 400) {
             var data = JSON.parse(request.responseText);
             if(data.albums.total === 0){
-                result_holder.innerHTML = "<h2>Results on "+query+"</h2>";
                 result_holder.innerHTML += "<p>Nothing Found</p>";
             }
             else{
-                result_holder.innerHTML = "<h2>Results on "+query+"</h2>";
                 for(var i = 0; i < data.albums.limit; i++){
-                    result_holder.innerHTML += "<div style=background-image:url("+data.albums.items[i].images[0].url+") data-album-id="+data.albums.items[i].id+" class=result>"+
+                    result_holder.innerHTML += "<div data-album-id="+data.albums.items[i].id+" class=result>"+
                         "<div id='artist'>"+
+                        "<img src='"+data.albums.items[i].images[0].url+"'>"+
                         "<h3>"+data.albums.items[i].name+"</h3>"+
                         "<p>"+data.albums.items[i].artists[0].name+"</p>"+
                         "</div>"+
