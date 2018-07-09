1319a19f7822d32d4df106a6324e76c667448a42
William <wsf235@gmail.com>
2018-07-09 14:37:35 -0400

    No idea why it... works now?

--- PaP5_Final/final/src/components/major.js
+++ PaP5_Final/final/src/components/major.js
@@ -4,6 +4,8 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 
+const url = process.env.PUBLIC_URL;
+
 class Major extends Component{
 
 
@@ -12,15 +14,15 @@ class Major extends Component{
         return(
             <nav className="major">
                 <ul>
-                    <li><NavLink to={"/featured/beef"}> Beef</NavLink></li>
-                    <li><NavLink to={"/featured/eggs"}> Eggs</NavLink></li>
-                    <li><NavLink to={"/featured/chicken"}> Chicken</NavLink></li>
-                    <li><NavLink to={"/featured/pork"}> Pork</NavLink></li>
-                    <li><NavLink to={"/featured/vegan"}> Vegan</NavLink></li>
-                    <li><NavLink to={"/featured/vegitable"}> Vegitable</NavLink></li>
-                    <li><NavLink to={"/featured/soup"}> Soup</NavLink></li>
-                    <li><NavLink to={"/featured/italian"}> Italian</NavLink></li>
-                    <li><NavLink to={"/featured/spanish"}> Spanish</NavLink></li>
+                    <li><NavLink to={url+"/featured/beef"}> Beef</NavLink></li>
+                    <li><NavLink to={url+"/featured/eggs"}> Eggs</NavLink></li>
+                    <li><NavLink to={url+"/featured/chicken"}> Chicken</NavLink></li>
+                    <li><NavLink to={url+"/featured/pork"}> Pork</NavLink></li>
+                    <li><NavLink to={url+"/featured/vegan"}> Vegan</NavLink></li>
+                    <li><NavLink to={url+"/featured/vegitable"}> Vegitable</NavLink></li>
+                    <li><NavLink to={url+"/featured/soup"}> Soup</NavLink></li>
+                    <li><NavLink to={url+"/featured/italian"}> Italian</NavLink></li>
+                    <li><NavLink to={url+"/featured/spanish"}> Spanish</NavLink></li>
                 </ul>
             </nav>
         );
