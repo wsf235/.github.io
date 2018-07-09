19b28082a883f9d77dd0a771bafab04c7321c411
William <wsf235@gmail.com>
2018-07-09 08:28:31 -0400

    Testing out adding a reactjs project

--- PaP5_Final/final/src/index.js
+++ PaP5_Final/final/src/index.js
@@ -5,5 +5,6 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log(process.env.PUBLIC_URL);
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
