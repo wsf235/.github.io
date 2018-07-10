d501a161c00069e64fb33a2ea6fecfe7e383754e
William <wsf235@gmail.com>
2018-07-10 09:26:14 -0400

    No idea why it... works now?

--- PaP5_Final/final/src/components/nav.js
+++ PaP5_Final/final/src/components/nav.js
@@ -8,13 +8,15 @@ import FaHome from 'react-icons/lib/ti/home-outline'
 import FaSearch from 'react-icons/lib/fa/search'
 import FaHeart from 'react-icons/lib/fa/heart'
 
+const url = process.env.PUBLIC_URL;
+
 class Nav extends Component{
     render(){
         return(
             <nav className="">
-                <NavLink to={"/featured"}><FaHome /> Home - Top Recipes</NavLink>
-                <NavLink to={"/search"}><FaSearch /> Search</NavLink>
-                <NavLink to={"/storage"}><FaHeart /> Your Recipes</NavLink>
+                <NavLink to={url+"/featured"}><FaHome /> Home - Top Recipes</NavLink>
+                <NavLink to={url+"/search"}><FaSearch /> Search</NavLink>
+                <NavLink to={url+"/storage"}><FaHeart /> Your Recipes</NavLink>
             </nav>
         );
     }
