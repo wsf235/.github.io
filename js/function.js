ed18c06dabf319b2835806e0b322498119bffb98
William <wsf235@gmail.com>
2018-08-16 16:31:40 -0400

    Nav Functionality upgrade

---
+++ js/function.js
@@ -0,0 +1,12 @@
+/**
+ * Created by MasterAnseen on 8/16/18.
+ */
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
